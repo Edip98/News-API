06778a1e55ace71d240357a67cecd0a2c7c02c46
Edip98 <edipkhalilov@gmail.com>
2022-10-14 16:19:01 +0300

    Initial commit

---
+++ UkraineNewsAPI/UkraineNewsAPI/NewsListView.swift
@@ -0,0 +1,21 @@
+//
+//  NewsListView.swift
+//  UkraineNewsAPI
+//
+//  Created by Эдип on 14.10.2022.
+//
+
+import SwiftUI
+
+struct NewsListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct NewsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsListView()
+    }
+}
---
+++ UkraineNewsAPI/UkraineNewsAPI/UkraineNewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UkraineNewsAPIApp.swift
+//  UkraineNewsAPI
+//
+//  Created by Эдип on 14.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct UkraineNewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NewsListView()
+        }
+    }
+}
